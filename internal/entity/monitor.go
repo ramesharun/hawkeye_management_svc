b04f399b3c58a1c969f4d13db624e9b311ab9ef8
ramesharun <ramesh95arun@yahoo.com>
2022-03-01 11:18:48 -0800

    Add files via upload

--- internal/entity/monitor.go
+++ internal/entity/monitor.go
@@ -1,1 +1,30 @@
+package entity
 
+import (
+	"time"
+)
+
+// Apichecks represents the API Monitor registered
+type Apichecks struct {
+	ID         string    `json:"id"`
+	Name       string    `json:"name"`
+	Monitor_ID string    `json:"monitor_id"`
+	OrgID      string    `json:"org_id"`
+	Tenant     string    `json:"tenant"`
+	IsDeleted  bool      `json:"is_deleted"`
+	UpdatedAt  time.Time `json:"last_ts"`
+}
+
+type TotalMonitorCount struct {
+	Count string `json:"total_monitors_count"`
+}
+
+type TotalMonitorCountForTenant struct {
+	TenantName string `json:"tenant_name"`
+	Count      string `json:"total_monitors_count"`
+}
+
+type TotalMonitorCountForOrg struct {
+	TenantName string `json:"org_name"`
+	Count      string `json:"total_monitors_count"`
+}
