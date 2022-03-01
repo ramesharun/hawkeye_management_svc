70e7c64a46aa538e0ad60d90ee0f57ab4a952ced
ramesharun <ramesh95arun@yahoo.com>
2022-03-01 11:22:09 -0800

    Add files via upload

---
+++ internal/healthcheck/api_test.go
@@ -0,0 +1,17 @@
+package healthcheck
+
+import (
+	"github.com/ramesharun/hawkeye-management-svc/internal/test"
+	"github.com/ramesharun/hawkeye-management-svc/pkg/log"
+	"net/http"
+	"testing"
+)
+
+func TestAPI(t *testing.T) {
+	logger, _ := log.NewForTest()
+	router := test.MockRouter(logger)
+	RegisterHandlers(router, "0.9.0")
+	test.Endpoint(t, router, test.APITestCase{
+		"ok", "GET", "/healthcheck", "", nil, http.StatusOK, `"OK 0.9.0"`,
+	})
+}
